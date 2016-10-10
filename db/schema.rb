a503453cc0937758e0e24c3eb9f99989838849b3
Matthew Lin <darthpwner@yahoo.com>
2016-10-10 06:05:54 +0000

    Added appropriate fields to databases, need to figure out how to make it foreign key as well as null

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161007221448) do
+ActiveRecord::Schema.define(version: 20161010060310) do
 
   create_table "sound_bytes", force: :cascade do |t|
     t.integer  "sb_id"
@@ -34,6 +34,10 @@ ActiveRecord::Schema.define(version: 20161007221448) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "username"
+    t.string   "user_id"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
