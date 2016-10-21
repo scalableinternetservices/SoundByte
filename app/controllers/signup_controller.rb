671480f359ac997b9527e3b76cfee04331b376ec
Matthew Lin <darthpwner@yahoo.com>
2016-10-21 23:04:32 +0000

    Set up devise stuff

--- app/controllers/signup_controller.rb
+++ app/controllers/signup_controller.rb
@@ -1,4 +1,6 @@
 class SignupController < ApplicationController
+    before_action :authenticate_user!, :configure_permitted_paramters
+    
     def configure_permitted_paramters
        devise_parameter_sanitizer.permit(:sign_up) do |user_params|
             user_params.permit({ roles: [] }, :email, :password, :password_confirmation)
