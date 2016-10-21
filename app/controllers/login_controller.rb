6ec5a739b7898e51685942b3c5fa251e8bdd417a
Matthew Lin <darthpwner@yahoo.com>
2016-10-21 22:26:02 +0000

    Try to figure out how to work with the login and signup actions

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -1,3 +1,9 @@
 class LoginController < ApplicationController
     before_action :authenticate_user!
+    
+    def configure_permitted_paramters
+        devise_parameter_sanitizer.permit(:sign_in) do |user_params|
+            user_params.permit(:username, :email) 
+        end
+    end
 end
--- app/controllers/signup_controller.rb
+++ app/controllers/signup_controller.rb
@@ -1,2 +1,8 @@
 class SignupController < ApplicationController
+    def configure_permitted_paramters
+       devise_parameter_sanitizer.permit(:sign_up) do |user_params|
+            user_params.permit({ roles: [] }, :email, :password, :password_confirmation)
+            format {render "signup"}
+        end 
+    end
 end
