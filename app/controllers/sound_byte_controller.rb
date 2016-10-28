005db6e3552a39f6bf80b7828e7ff83a234ba29e
Matthew Lin <darthpwner@yahoo.com>
2016-10-28 22:20:15 +0000

    Work on the sound_bte_controller

--- app/controllers/sound_byte_controller.rb
+++ app/controllers/sound_byte_controller.rb
@@ -1,2 +1,25 @@
 class SoundByteController < ApplicationController
+    def index
+        @sound_bytes = SoundByte.order('created_at')
+    end
+    
+    def new
+        @sound_byte = SoundByte.new
+    end
+
+    def create
+        @sound_byte = SoundByte.new(sound_byte_params)
+        if @sound_byte.save
+            flash[:success] = "The photo was added!"
+            redirect_to root_path
+        else
+            render 'new'
+        end
+    end
+
+    private
+    
+    def sound_byte_params
+        # params.require(:sound_byte).permit(:)
+    end
 end
