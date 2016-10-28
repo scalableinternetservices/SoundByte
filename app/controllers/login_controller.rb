22e4795a2f3d4ea7426f9a9c08102f806ca4e042
Matthew Lin <darthpwner@yahoo.com>
2016-10-28 03:23:29 +0000

    Prints out current logged in user for proof

--- app/controllers/login_controller.rb
+++
@@ -1,9 +0,0 @@
-class LoginController < ApplicationController
-    before_action :authenticate_user!
-    
-    def configure_permitted_paramters
-        devise_parameter_sanitizer.permit(:sign_in) do |user_params|
-            user_params.permit(:username, :email) 
-        end
-    end
-end
