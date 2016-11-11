ca5b26519864bd5c091dfe2aab520f629a3f4eed
kfrankc <frankchen1698@gmail.com>
2016-11-11 00:57:04 +0000

    feature: changed UI color for hovering

--- db/migrate/20161010060310_add_string_to_users.rb
+++ db/migrate/20161010060310_add_string_to_users.rb
@@ -1,6 +1,6 @@
 class AddStringToUsers < ActiveRecord::Migration
   def change
       # add_column :users, :user_id, :string
-      add_column :users, :age, :string
+      # add_column :users, :age, :string
   end
 end
