6d5f6a797adf2767fe7f63cdb6a647f396a63009
Matthew Lin <darthpwner@yahoo.com>
2016-11-13 00:19:25 +0000

    Added ability to delete songs

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  resources :sound_bytes, only: [:new, :create, :index, :destroy]
+  resources :sound_bytes, only: [:new, :create, :index, :destroy, :delete]
   
   root 'pages#home'
   
--- db/migrate/20161010060310_add_string_to_users.rb
+++ db/migrate/20161010060310_add_string_to_users.rb
@@ -1,10 +1,5 @@
 class AddStringToUsers < ActiveRecord::Migration
   def change
-<<<<<<< HEAD
-      # add_column :users, :user_id, :string
-      add_column :users, :age, :string
-=======
       add_column :users, :age, :integer
->>>>>>> fa21596ceea33639a089e74c4416dc980bce8ac3
   end
 end
